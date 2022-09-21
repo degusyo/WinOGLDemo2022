122e73b1c6cef22cceb7b340d63eb7569ffb43cf
翔馬 出口 <m1m21a33@oit.ac.jp>
2022-09-21 12:38:10 +0900

    問6.1

--- WinOGLDemo2022/WinOGLDemo2022View.cpp
+++ WinOGLDemo2022/WinOGLDemo2022View.cpp
@@ -67,9 +67,10 @@ void CWinOGLDemo2022View::OnDraw(CDC* pDC)
 	// この間に描画を命令
 	glColor3f(1.0, 1.0, 1.0);
 	glBegin(GL_LINE_LOOP);
-	glVertex2f(-1.0, 0.5);
-	glVertex2f(0.0, -0.5);
-	glVertex2f(1.0, 0.5);
+	glVertex2f(-0.5, 0.5);
+	glVertex2f(-0.5, -0.5);
+	glVertex2f(0.5, -0.5);
+	glVertex2f(0.5, 0.5);
 	glEnd();
 
 	glFlush();
